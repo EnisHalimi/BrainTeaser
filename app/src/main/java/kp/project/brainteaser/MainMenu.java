031f6942edb68bf7ce258fa067c2db8e5ee93483
KPTeam <kp-team@hotmail.com>
2017-04-13 13:55:06 +0200

    Small changes

--- app/src/main/java/kp/project/brainteaser/MainMenu.java
+++ app/src/main/java/kp/project/brainteaser/MainMenu.java
@@ -1,11 +1,13 @@
 package kp.project.brainteaser;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainMenu extends AppCompatActivity {
@@ -14,15 +16,30 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
-        final TextView mContentView = (TextView) findViewById(R.id.textView);
-        Button button = (Button) findViewById(R.id.dummy_button);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button playButton = (Button) findViewById(R.id.playButton);
+        Button achievementsButton = (Button) findViewById(R.id.achivmentsButton);
+        Button optionsButton = (Button) findViewById(R.id.optionsButton);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mContentView.getVisibility() == View.INVISIBLE)
-                    mContentView.setVisibility(View.VISIBLE);
-                else
-                    mContentView.setVisibility(View.INVISIBLE);
+                Toast.makeText(MainMenu.this, "Pressed Play Button", Toast.LENGTH_LONG).show();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        achievementsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                Toast.makeText(MainMenu.this, "Pressed Play Button", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        optionsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                Toast.makeText(MainMenu.this, "Pressed Play Button", Toast.LENGTH_LONG).show();
             }
         });
     }
